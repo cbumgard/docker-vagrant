5d75dd1c54ea5ad2781cf0b4b6e9cb07bff759e1
Chris Bumgardner <cbumgard@gmail.com>
2014-02-20 10:50:23 -0800

    non-root access to docker

--- sh/docker.sh
+++ sh/docker.sh
@@ -3,6 +3,11 @@
 # From: http://docs.docker.io/en/latest/installation/ubuntulinux/
 curl -s https://get.docker.io/ubuntu/ | sudo sh
 
+# Non-root docker access: create a docker group; add the vagrant user; restart docker:
+groupadd docker
+gpasswd -a vagrant docker
+service docker restart
+
 echo "Test out docker with the following commands:"
 echo
 echo "vagrant ssh"
